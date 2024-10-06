ea9daaf288c0415240da9c5ea1ec8ccdcf640dad
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-06 11:38:40 +0100

    updated file

--- src/run.sh
+++ src/run.sh
@@ -31,7 +31,7 @@ echo "Crawler run complete!"
 echo "Starting DVC operations..."
 
 # Add new data to DVC
-dvc add data/raw/
+dvc add data/raw/transfermarkt/
 
 # Commit changes to DVC
 dvc commit
@@ -39,16 +39,16 @@ dvc commit
 # Push to remote storage
 dvc push
 
-# # Git operations (optional, but recommended)
-# echo "Starting Git operations..."
-#
-# # Stage DVC file changes
-# git add data.dvc
-#
-# # Commit DVC file changes
-# git commit -m "Update data: $SCRIPT crawler run on $(date)"
-#
-# # Push Git changes (if you want to push to a Git remote)
-# # git push
+# Git operations (optional, but recommended)
+echo "Starting Git operations..."
+
+# Stage DVC file changes
+git add data/raw/transfermarkt/
+
+# Commit DVC file changes
+git commit -m "Update data: $SCRIPT crawler run on $(date)"
+
+# Push Git changes (if you want to push to a Git remote)
+git push -u origin master
 
 echo "DVC operations complete!"
