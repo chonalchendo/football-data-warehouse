d797d7249225f410056f20e6b02342dbe8198a8b
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-07 20:50:03 +0100

    changed configuration to be compatible with docker

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Set the path to your Python interpreter
-PYTHON_PATH=$(which python3)
-
-# Set the path to your project directory
-PROJECT_DIR="$HOME/Development/football-app/football-data-warehouse"
-
-# Change to the project directory
-cd "$PROJECT_DIR"
+# Use the Python interpreter available in the container's PATH
+PYTHON="python"
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 <script> [additional arguments...]"
@@ -22,9 +16,8 @@ shift  # Remove the first argument from the argument list
 echo "Running $SCRIPT crawler..."
 
 # Run the scraper with remaining arguments
-$PYTHON_PATH -m src.extractors.$SCRIPT "$@"
+$PYTHON -m src.extractors.$SCRIPT "$@"
 
-# Print a message when done
 echo "Crawler run complete!"
 
 # DVC operations
