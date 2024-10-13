9a143a3c41321fbdcb8fe14187ecc110d69c7b69
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-13 14:29:13 +0100

    created and specified path to dbt project

--- orchestrator/orchestrator/project.py
+++ orchestrator/orchestrator/project.py
@@ -1,3 +1,14 @@
-"""
-For future DBT pipeline which will integrate with the project.
-"""
+from pathlib import Path
+
+from dagster_dbt import DbtProject 
+
+
+relative_path = (Path(__file__).parent.parent.parent / 'dbt_pipeline').resolve()
+
+dbt_project = DbtProject(
+        project_dir=relative_path,
+)
+dbt_project.prepare_if_dev()
+
+
+
