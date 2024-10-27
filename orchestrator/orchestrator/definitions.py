2363410b33a79a9c3554a28a3b4645df263756eb
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-27 18:51:24 +0000

    updated to load all assets from each asset module

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -1,16 +1,22 @@
-from dagster import Definitions
+from dagster import Definitions, load_assets_from_package_module, load_assets_from_modules
 from dagster_dbt import DbtCliResource
 
 from .assets.dbt import my_dbt_assets
-from .assets.transfermarkt import squads
-from .assets.fbref import player_defense 
+from .assets import seeds, fbref, transfermarkt
 from .project import dbt_project
+
+
+fbref_assets = load_assets_from_package_module(fbref, group_name="fbref")
+seed_assets = load_assets_from_modules([seeds], group_name="seeds")
+transfermarkt_assets = load_assets_from_modules([transfermarkt], group_name="transfermarkt")
 
 resources = {
     "dbt": DbtCliResource(project_dir=dbt_project),
 }
 
 defs = Definitions(
-    assets=[squads, player_defense, my_dbt_assets],
+    assets=[*transfermarkt_assets, *fbref_assets, *seed_assets, my_dbt_assets],
     resources=resources,
 )
+
+
