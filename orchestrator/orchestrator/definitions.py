66eade4771937ae2c3f4dc108cfcb393b0779e2a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-30 19:42:52 +0000

    changed the import for fbref assets

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -1,12 +1,15 @@
-from dagster import Definitions, load_assets_from_package_module, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules 
 from dagster_dbt import DbtCliResource
 
 from .assets.dbt import my_dbt_assets
-from .assets import seeds, fbref, transfermarkt
+from .assets import seeds, transfermarkt
+from .assets.fbref import generate_fbref_asset 
 from .project import dbt_project
+from .constants import fbrefcollectors 
 
 
-fbref_assets = load_assets_from_package_module(fbref, group_name="fbref")
+
+fbref_assets = [generate_fbref_asset(collector) for collector in fbrefcollectors]
 seed_assets = load_assets_from_modules([seeds], group_name="seeds")
 transfermarkt_assets = load_assets_from_modules([transfermarkt], group_name="transfermarkt")
 
