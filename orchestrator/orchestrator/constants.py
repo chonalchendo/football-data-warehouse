629cbe71a0227452d09ee089c51ad4bbc7d1a28c
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-10-30 19:42:26 +0000

    added list of fbref collector names

--- orchestrator/orchestrator/constants.py
+++ orchestrator/orchestrator/constants.py
@@ -5,18 +5,17 @@ class FbrefConfig(Config):
     season: str
 
 
-class FbrefCollector:
-    DEFENSE = "player_defense"
-    KEEPER = "player_keeper"
-    KEEPER_ADVANCED = "player_keeper_adv"
-    PASSING = "player_passing"
-    PASSING_TYPE = "player_passing_type"
-    POSSESSION = "player_possession"
-    PLAYING_TIME = "player_playing_time"
-    SHOOTING = "player_shooting"
-    STANDARD = "player_standard"
-    GCA = "player_gca"
-    MISC = "player_misc"
-
-
+fbrefcollectors = [
+    'player_defense',
+    'player_misc',
+    'player_passing',
+    'player_possession',
+    'player_shooting',
+    'player_keeper',
+    'player_keeper_adv',
+    'player_playing_time',
+    'player_standard_stats',
+    'player_passing_type',
+    'player_gca'
+]
 
