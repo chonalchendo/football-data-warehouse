6714b4a8cee6f07b4a5d64056f7e0bb96977f4e3
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-01 15:36:28 +0000

    reformatted files

--- orchestrator/orchestrator/assets/fbref.py
+++ orchestrator/orchestrator/assets/fbref.py
@@ -1,39 +1,38 @@
 import polars as pl
-from dagster import (AssetExecutionContext, MaterializeResult, MetadataValue,
-                     asset)
+from dagster import AssetExecutionContext, MaterializeResult, MetadataValue, asset
 
 from src.extractors.fbref import run_crawler
 
-from ..constants import FbrefConfig
-
+from ..constants import season_partitions
 
 
 def generate_fbref_asset(collector: str):
     @asset(
-        name=collector, 
-        compute_kind="python", 
+        name=collector,
+        compute_kind="python",
         description=f"{collector} stats crawled from FBRef",
-        group_name='fbref'
+        group_name="fbref",
+        partitions_def=season_partitions,
     )
-    def _asset(
-        context: AssetExecutionContext, config: FbrefConfig
-    ) -> MaterializeResult:
-        context.log.info("Creating keeper stats asset")
+    def _asset(context: AssetExecutionContext) -> MaterializeResult:
 
-        run_crawler(collector, config.season)
+        season = context.partition_key
 
-        context.log.info(f"Keeper stats asset scraped for season {config.season}")
+        context.log.info(f"Creating {collector} asset for season {season}")
 
-        output_path = f"data/raw/fbref/{config.season}/{collector}.json.gz"
-        df = pl.read_ndjson(output_path)
+        run_crawler(collector, season)
+
+        context.log.info(f"Keeper stats asset scraped for season {season}")
+
+        output_path = f"data/raw/fbref/{season}/{collector}.parquet"
+        df = pl.read_parquet(output_path)
 
         return MaterializeResult(
             metadata={
                 "num_records": len(df),
-                "season": config.season,
+                "season": season,
                 "preview": MetadataValue.md(df.to_pandas().head().to_markdown()),
             }
-        )   
+        )
 
     return _asset
-
