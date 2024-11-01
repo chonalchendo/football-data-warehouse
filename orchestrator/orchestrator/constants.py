457146192a8b43f06cc96c1c6473173913846257
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-01 15:36:53 +0000

    added seasons

--- orchestrator/orchestrator/constants.py
+++ orchestrator/orchestrator/constants.py
@@ -1,4 +1,4 @@
-from dagster import Config
+from dagster import Config, StaticPartitionsDefinition
 
 
 class FbrefConfig(Config):
@@ -19,3 +19,6 @@ fbrefcollectors = [
     'player_gca'
 ]
 
+
+seasons = ['2018', '2019', '2020', '2021', '2022', '2023']
+season_partitions = StaticPartitionsDefinition(seasons)
