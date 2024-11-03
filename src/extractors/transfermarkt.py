e2a0aef2f6382288ad063c369640ff9664f3365b
chonalchendo <conalhenderson@gmail.com>
2024-11-03 18:24:21 +0000

    adapted to new pydantic settings

--- src/extractors/transfermarkt.py
+++ src/extractors/transfermarkt.py
@@ -1,15 +1,18 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
+from rich import print
 
-from utils import read_config
+from .settings import get_config
 
 
 def set_scrapy_settings() -> Settings:
     settings = get_project_settings()
-    config = read_config()
+    config = get_config()
 
-    scrapy_settings: dict = config["transfermarkt_extract"]["scrapy_config"]
+    scrapy_settings = config.transfermarkt_extract.model_dump(exclude=None)
+    
+    print(scrapy_settings)
 
     settings.setdict(scrapy_settings)
     return settings
@@ -20,3 +23,7 @@ def run_spider(crawler: str, season: str) -> None:
     process = CrawlerProcess(settings)
     process.crawl(crawler, season=season)
     process.start()
+
+
+if __name__ == "__main__":
+    run_spider("squads", "2021")
