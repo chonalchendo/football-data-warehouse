693828b1295b86f600286b8647eb5292353aef44
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-11 17:59:42 +0000

    reformatted files using new make command

--- src/extractors/settings.py
+++ src/extractors/settings.py
@@ -8,26 +8,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class TransfermarktConfig(BaseSettings):
     SPIDER_MODULES: list[str] = ["transfermarkt"]
     NEWSPIDER_MODULE: list[str] = ["transfermarkt"]
-    USER_AGENT: str = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    )
+    USER_AGENT: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
     ROBOTSTXT_OBEY: bool = True
     DOWNLOAD_DELAY: int = 1
     COOKIES_ENABLED: bool = False
     REQUEST_FINGERPRINTER_IMPLEMENTATION: str = "2.7"
     TWISTED_REACTOR: str = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
     FEED_EXPORT_ENCODING: str = "utf-8"
-    ITEM_PIPELINES: dict[str, int] = Field(
-        default_factory=lambda: {
-            "transfermarkt.transfermarkt.pipelines.TransfermarktGCSPipeline": 300
-        }
-    )
+    ITEM_PIPELINES: dict[str, int] = Field(default_factory=lambda: {"transfermarkt.transfermarkt.pipelines.TransfermarktGCSPipeline": 300})
     FEEDS: dict[str, Any] = Field(
-        default_factory=lambda: {
-            "gs://football-data-warehouse/raw/transfermarkt/{season}/{name}.parquet": {
-                "format": "parquet"
-            }
-        }
+        default_factory=lambda: {"gs://football-data-warehouse/raw/transfermarkt/{season}/{name}.parquet": {"format": "parquet"}}
     )
 
     model_config = SettingsConfigDict(
@@ -62,9 +52,7 @@ class FBRefSettings(BaseSettings):
 
 
 class ScraperConfig(BaseSettings):
-    transfermarkt_extract: TransfermarktConfig = Field(
-        default_factory=TransfermarktConfig
-    )
+    transfermarkt_extract: TransfermarktConfig = Field(default_factory=TransfermarktConfig)
     fbref_extract: FBRefSettings = Field(default_factory=FBRefSettings)
 
 
