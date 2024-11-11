78f9f41e5f12a3e0bd210f507ad18af3cb74bc45
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-11 18:37:42 +0000

    entrypoint for dockerfile to copy all files from the directory to the image

---
+++ src/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+PROJECT_HOME=/app/football-data-warehouse
+BRANCH=$1
+COMMAND=$2
+
+shift 2
+
+if ! [ -d $PROJECT_HOME ]; then
+    echo "Setting up project dir"
+    if ! [ -z ${BRANCH+x} ]; then
+        git clone --branch $BRANCH https://github.com/chonalchendo/football-data-warehouse.git
+        cd $PROJECT_HOME
+    else
+        echo "BRANCH is required to bootstrap the environment"
+        exit 1
+    fi
+fi
+
+cd $PROJECT_HOME
+if $COMMAND "$@" ; then
+    echo "Job succeeded"
+    exit 0
+else
+    echo "Job failed"
+    exit 1
+fi
