172a9f783ab3c5d1be2471b638c88f44abcf4e4a
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-18 21:04:11 +0000

    created fbref wage asset

--- orchestrator/orchestrator/assets/fbref.py
+++ orchestrator/orchestrator/assets/fbref.py
@@ -6,38 +6,71 @@ from dagster import (
     asset,
 )
 
-from src.extractors.fbref import run_crawler
+from src.extractors.fbref import run_stats_crawler, run_wage_crawler
+from src.extractors.settings import get_config
 
-from ..constants import season_partitions
-from ..resources import get_gcs_storage_options
+from ..constants import SEASON_PARTITIONS
 
 
-def generate_fbref_asset(collector: str):
+def generate_fbref_stat_asset(collector: str):
     @asset(
         name=collector,
         compute_kind="python",
-        description=f"{collector} stats crawled from FBRef",
+        description=f"{collector} stats crawled from Fbref",
         group_name="fbref",
-        partitions_def=season_partitions,
+        partitions_def=SEASON_PARTITIONS,
     )
     def _asset(context: AssetExecutionContext) -> MaterializeResult:
         season = context.partition_key
 
         context.log.info(f"Creating {collector} asset for season {season}")
 
-        run_crawler(collector, season)
+        run_stats_crawler(collector, season)
 
         context.log.info(f"Keeper stats asset scraped for season {season}")
 
-        output_path = f"gs://football-data-warehouse/raw/fbref/{season}/{collector}.parquet"
-        df = pl.read_parquet(output_path, storage_options=get_gcs_storage_options())
+        output_path = f"data/raw/fbref/{season}/{collector}.parquet"
+        df = pl.read_parquet(output_path)
 
         return MaterializeResult(
             metadata={
-                "num_records": len(df),
+                "records": len(df),
                 "season": season,
                 "preview": MetadataValue.md(df.to_pandas().head().to_markdown()),
             }
         )
 
     return _asset
+
+
+@asset(
+    name="player_wages",
+    compute_kind="python",
+    description="Player wages crawled from Fbref",
+    partitions_def=SEASON_PARTITIONS,
+    group_name="fbref",
+)
+def fbref_wage_asset(context: AssetExecutionContext) -> MaterializeResult:
+    season = context.partition_key
+
+    context.log.info(f"Creating fbref wage asset for season {season}")
+
+    settings = get_config().fbref_extract
+    comps = settings.COMPS
+
+    for comp, id in comps.items():
+        context.log.info(
+            f"Collecting wage data for: league - {comp} & season - {season}"
+        )
+        run_wage_crawler(comp_id=id, comp_name=comp, season=season)
+
+    output_path = f"data/raw/fbref/{season}/player_wage*.parquet"
+    df = pl.read_parquet(output_path)
+
+    return MaterializeResult(
+        metadata={
+            "records": len(df),
+            "season": season,
+            "preview": MetadataValue.md(df.to_pandas().head().to_markdown()),
+        }
+    )
