b34a09f6ba72a5975436c7a37c130cd23f8d765c
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-18 21:01:20 +0000

    renamed various assets

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -3,11 +3,13 @@ from dagster_dbt import DbtCliResource
 
 from .assets import seeds, transfermarkt
 from .assets.dbt import my_dbt_assets
-from .assets.fbref import generate_fbref_asset
-from .constants import fbrefcollectors
+from .assets.fbref import generate_fbref_stat_asset, fbref_wage_asset
+from .constants import FBREF_STATS_COLLECTORS
 from .project import dbt_project
 
-fbref_assets = [generate_fbref_asset(collector) for collector in fbrefcollectors]
+fbref_stat_assets = [
+    generate_fbref_stat_asset(collector) for collector in FBREF_STATS_COLLECTORS
+]
 seed_assets = load_assets_from_modules([seeds], group_name="seeds")
 transfermarkt_assets = load_assets_from_modules(
     [transfermarkt], group_name="transfermarkt"
@@ -21,6 +23,12 @@ RESOURCES = {
 }
 
 defs = Definitions(
-    assets=[*transfermarkt_assets, *fbref_assets, *seed_assets, my_dbt_assets],
+    assets=[
+        *transfermarkt_assets,
+        *fbref_stat_assets,
+        fbref_wage_asset,
+        *seed_assets,
+        my_dbt_assets,
+    ],
     resources=RESOURCES,
 )
