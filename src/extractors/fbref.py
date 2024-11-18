81fd05c4ef1c4bfde2fa8530c9aaf088d94251f7
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-18 21:05:20 +0000

    created function to run wage crawler

--- src/extractors/fbref.py
+++ src/extractors/fbref.py
@@ -5,7 +5,7 @@ from fbref.fbref import NavigatorRunner, ParquetFeed
 from .settings import get_config
 
 
-def run_crawler(collector: str, season: str) -> None:
+def run_stats_crawler(collector: str, season: str) -> None:
     settings = get_config().fbref_extract
     feed = ParquetFeed(
         output_path=settings.FEEDS.PATH,
@@ -16,10 +16,26 @@ def run_crawler(collector: str, season: str) -> None:
     runner.start()
 
 
-if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("--collector", type=str, required=True)
-    arg_parser.add_argument("--season", type=str, required=True)
-    args = arg_parser.parse_args()
+def run_wage_crawler(comp_id: int, comp_name: str, season: str) -> None:
+    settings = get_config().fbref_extract
+    params = {"comp_id": comp_id, "comp_name": comp_name}
+    feed = ParquetFeed(
+        output_path=settings.FEEDS.PATH, format=settings.FEEDS.FORMAT
+    )
+    runner = NavigatorRunner(feed=feed, download_delay=10)
+    runner.navigate(collector="player_wages", params=params, season=season)
+    runner.start()
 
-    run_crawler(collector=args.collector, season=args.season)
+
+if __name__ == "__main__":
+    # arg_parser = argparse.ArgumentParser()
+    # arg_parser.add_argument("--collector", type=str, required=True)
+    # arg_parser.add_argument("--season", type=str, required=True)
+    # args = arg_parser.parse_args()
+
+    # run_stats_crawler(collector=args.collector, season=args.season)
+    settings = get_config().fbref_extract
+    comps = settings.COMPS
+    
+    for comp, id in comps.items():
+        run_wage_crawler(comp_id=id, comp_name=comp, season=2021)
