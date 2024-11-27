a830649fdbab6c86f9be3778819dc154de36add4
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-27 20:15:42 +0000

    added jobs

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -6,11 +6,12 @@ from .assets.dbt import my_dbt_assets
 from .assets.fbref import generate_fbref_stat_asset, fbref_wage_asset
 from .constants import FBREF_STATS_COLLECTORS
 from .project import dbt_project
+from .jobs import team_mapping_job, transfermarkt_raw_assets, fbref_raw_stats_assets
 
 fbref_stat_assets = [
     generate_fbref_stat_asset(collector) for collector in FBREF_STATS_COLLECTORS
 ]
-seed_assets = load_assets_from_modules([seeds], group_name="seeds")
+seed_assets = load_assets_from_modules([seeds])
 transfermarkt_assets = load_assets_from_modules(
     [transfermarkt], group_name="transfermarkt"
 )
@@ -31,4 +32,5 @@ defs = Definitions(
         my_dbt_assets,
     ],
     resources=RESOURCES,
+    jobs=[team_mapping_job, transfermarkt_raw_assets, fbref_raw_stats_assets],
 )
