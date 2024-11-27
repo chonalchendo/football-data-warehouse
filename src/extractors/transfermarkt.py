6b4b756190431921d285c61d8ff5c6f46c1b499b
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-27 20:17:00 +0000

    created asset specific functions for crawling fbref pages

--- src/extractors/transfermarkt.py
+++ src/extractors/transfermarkt.py
@@ -1,4 +1,6 @@
 from rich import print
+from pathlib import Path
+import polars as pl
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from scrapy.utils.project import get_project_settings
@@ -18,12 +20,26 @@ def set_scrapy_settings() -> Settings:
     return settings
 
 
-def run_spider(crawler: str, season: str) -> None:
+def run_clubs_spider(crawler: str, season: str) -> None:
     settings = set_scrapy_settings()
     process = CrawlerProcess(settings)
-    process.crawl(crawler, season=season)
+
+    process.crawl(crawler, season=season, leagues="all")
+    process.start()
+
+
+def run_squads_spider(crawler: str, season: str) -> None:
+    settings = set_scrapy_settings()
+    process = CrawlerProcess(settings)
+
+    clubs_path = Path(f"data/raw/transfermarkt/{season}/clubs.parquet").resolve()
+    clubs_df = pl.read_parquet(clubs_path, use_pyarrow=True)
+
+    clubs = clubs_df.to_dicts()
+
+    process.crawl(crawler, season=season, clubs=clubs)
     process.start()
 
 
 if __name__ == "__main__":
-    run_spider("squads", "2021")
+    run_squads_spider(crawler="squads", season="2024")
