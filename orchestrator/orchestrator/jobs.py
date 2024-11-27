1678f567ce69cb2ab47f79be405fc2ae86d86292
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-27 20:15:34 +0000

    created jobs to run specific parts of the pipeline

---
+++ orchestrator/orchestrator/jobs.py
@@ -0,0 +1,20 @@
+from dagster import define_asset_job, in_process_executor
+from .constants import FBREF_STATS_COLLECTORS
+
+team_mapping_job = define_asset_job(
+    name="team_mapping_job",
+    selection=["squads", "player_defense", "team_mapping_asset"],
+    executor_def=in_process_executor,
+)
+
+transfermarkt_raw_assets = define_asset_job(
+    name="transfermarkt_raw_assets",
+    selection=["squads"],
+    # executor_def=in_process_executor,
+)
+
+fbref_raw_stats_assets = define_asset_job(
+    name="fbref_raw_stats_assets",
+    selection=FBREF_STATS_COLLECTORS,
+    executor_def=in_process_executor,
+)
