b56344f37937ac62c6f0449caa57056ee363838d
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-11-29 19:15:49 +0000

    sorted issue with N/A values in clean_height cte

--- dbt/models/staging/transfermarkt/stg_transfermarkt__squads.sql
+++ dbt/models/staging/transfermarkt/stg_transfermarkt__squads.sql
@@ -28,9 +28,10 @@ clean_market_values AS (
             WHEN value LIKE '%bn' THEN REPLACE(REPLACE(value, '€', ''), 'bn', '') :: FLOAT * 1000
             WHEN value = '-' THEN NULL
             ELSE REPLACE(value, '€', '') :: FLOAT / 1000000
-        END AS market_value_euro_mill
+        END AS market_value_eur_mill
     FROM clean_squad_numbers 
 ),
+
 
 -- Clean signing fee
 clean_signing_fee AS (
@@ -43,16 +44,19 @@ clean_signing_fee AS (
             WHEN signing_fee IN ('-', '?', 'free transfer', 'draft') THEN 0
             WHEN signing_fee = '' THEN NULL
             ELSE REPLACE(REPLACE(signing_fee, '€', ''), 'Ablöse ', '') :: FLOAT / 1000000
-        END AS signing_fee_euro_mill
+        END AS signing_fee_eur_mill
     FROM clean_market_values
 ),
+
+
 
 -- Clean height
 clean_height AS (
     SELECT
         *,
         CASE 
-            WHEN height = '-' THEN 0
+            WHEN height = '-' THEN NULL
+            WHEN height LIKE '%N/A%' THEN NULL
             ELSE REPLACE(REPLACE(height, 'm', ''), ',', '.') :: FLOAT
         END AS clean_height
     FROM clean_signing_fee 
@@ -73,8 +77,8 @@ clean_position AS (
 SELECT
     tm_id :: INTEGER as tm_id,
     tm_name,
-    name,
-    season,
+    name as player,
+    season :: INTEGER as season,
     country,
     foot,
     age :: INTEGER as age,
@@ -83,8 +87,8 @@ SELECT
     current_club,
     signed_from,
     clean_position AS position,
-    clean_squad_num :: INTEGER AS squad_num,
-    market_value_euro_mill,
-    signing_fee_euro_mill,
+    clean_squad_num :: INTEGER AS squad_number,
+    market_value_eur_mill,
+    signing_fee_eur_mill,
     clean_height AS height
 FROM clean_position
