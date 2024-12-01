19ae92986a91d150dd60294bffe820cf805a39a6
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-12-01 18:00:07 +0000

    called manifest path function

--- orchestrator/orchestrator/assets/dbt.py
+++ orchestrator/orchestrator/assets/dbt.py
@@ -6,6 +6,6 @@ from ..project import get_dbt_manifest_path
 # from ..project import dbt_project
 
 
-@dbt_assets(manifest=get_dbt_manifest_path)
+@dbt_assets(manifest=get_dbt_manifest_path())
 def my_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
