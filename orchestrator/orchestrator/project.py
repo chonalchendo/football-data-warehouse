e22176d814c529193b4d1199b41940795ee608e7
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-12-01 17:38:16 +0000

    created function to get dbt manifest path

--- orchestrator/orchestrator/project.py
+++ orchestrator/orchestrator/project.py
@@ -1,14 +1,37 @@
 import os
 from pathlib import Path
 
-from dagster_dbt import DbtProject
+# from dagster_dbt import DbtProject
+from dagster_dbt import DbtCliResource
 
-default_path = (Path(__file__).parent.parent.parent).resolve()
+dbt_project_dir = (Path(__file__).joinpath("../..", "dbt")).resolve()
 
-root_path = os.environ.get("ROOT_PATH", default_path)
-relative_path = os.path.join(root_path, "dbt")
-
-dbt_project = DbtProject(
-    project_dir=relative_path,
+dbt = DbtCliResource(
+    project_dir=os.fspath(dbt_project_dir),
+    profiles_dir=os.fspath(dbt_project_dir),
 )
-dbt_project.prepare_if_dev()
+
+
+def get_dbt_manifest_path() -> Path:
+    if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+        dbt_parse_invocation = dbt.cli(["parse"], manifest={}).wait()
+        dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+    else:
+        dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
+    return dbt_manifest_path
+
+
+# root_path = os.environ.get("ROOT_PATH", default_path)
+# relative_path = os.path.join(root_path, "dbt")
+
+# def get_manifest_path() -> Path:
+#     if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+#         dbt_parse_invocation = dbt.cli(["parse"], manifest={}).wait()
+#         dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+#     else:
+#         dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
+#     return dbt_manifest_path
+# dbt_project = DbtProject(
+#     project_dir=dbt_project_dir,
+# )
+# dbt_project.prepare_if_dev()
