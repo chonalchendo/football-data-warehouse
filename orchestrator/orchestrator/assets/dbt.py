78eccf521ee1b9fddd7dcd3eb01d5b5f2e2bee2d
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-12-01 17:39:40 +0000

    added function to get manifest path

--- orchestrator/orchestrator/assets/dbt.py
+++ orchestrator/orchestrator/assets/dbt.py
@@ -1,9 +1,11 @@
 from dagster import AssetExecutionContext
 from dagster_dbt import DbtCliResource, dbt_assets
 
-from ..project import dbt_project
+from ..project import get_dbt_manifest_path
+
+# from ..project import dbt_project
 
 
-@dbt_assets(manifest=dbt_project.manifest_path)
+@dbt_assets(manifest=get_dbt_manifest_path)
 def my_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
