5580ba68ddd4b3ac89d5467909a6fc63ff6ae371
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2024-12-08 21:13:24 +0000

    added resources and scheduler

--- orchestrator/orchestrator/definitions.py
+++ orchestrator/orchestrator/definitions.py
@@ -1,43 +1,58 @@
+import os
+
 from dagster import Definitions, load_assets_from_modules
+from dagster_dbt import DbtCliResource
 
 from .assets import seeds, transfermarkt
 from .assets.dbt import my_dbt_assets
-from .assets.fbref import fbref_wage_asset, generate_fbref_stat_asset
+from .assets.fbref import generate_fbref_stat_asset, player_wages
 from .constants import FBREF_STATS_COLLECTORS
-from .jobs import (all_raw_assets_job, fbref_raw_stats_assets,
-                   team_mapping_job, transfermarkt_raw_assets)
-# from .project import dbt_project
-from .resources import RESOURCES
-
-# from dagster_dbt import DbtCliResource
-
+from .jobs import (fbref_raw_stats_assets, fbref_raw_stats_schedule,
+                   transfermarkt_raw_assets, transfermarkt_raw_schedule)
+from .project import dbt_project_dir
+from .resources import S3PartitionedParquetIOManager
 
 fbref_stat_assets = [
     generate_fbref_stat_asset(collector) for collector in FBREF_STATS_COLLECTORS
 ]
 seed_assets = load_assets_from_modules([seeds])
 transfermarkt_assets = load_assets_from_modules(
-    [transfermarkt], group_name="transfermarkt"
+    [transfermarkt],
+    group_name="transfermarkt",
 )
 
+STORAGE_OPTIONS = {
+    "aws_secret_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
+    "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
+    "aws_region": "us-east-1",
+}
 
-# RESOURCES = {
-#     "dbt": DbtCliResource(project_dir=dbt_project),
-# }
+
+RESOURCES = {
+    "dbt": DbtCliResource(project_dir=dbt_project_dir),
+    "parquet_io_manager": S3PartitionedParquetIOManager(
+        s3_bucket="football-data-warehouse",
+        storage_options=STORAGE_OPTIONS,
+    ),
+    # "warehouse_io_manager": S3DuckDBPartitionedIOManager(
+    #     duckdb_path=str(dbt_project_dir / "duckdb" / "database.db"),
+    #     s3_bucket="football-data-warehouse",
+    #     storage_options=STORAGE_OPTIONS,
+    # ),
+}
 
 defs = Definitions(
     assets=[
         *transfermarkt_assets,
         *fbref_stat_assets,
-        fbref_wage_asset,
+        player_wages,
         *seed_assets,
         my_dbt_assets,
     ],
     resources=RESOURCES,
+    schedules=[fbref_raw_stats_schedule, transfermarkt_raw_schedule],
     jobs=[
-        team_mapping_job,
         transfermarkt_raw_assets,
         fbref_raw_stats_assets,
-        all_raw_assets_job,
     ],
 )
