e732e2bdec6cf3d63cb2fb4de7b0344f04e441df
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-07 15:08:02 +0000

    added pydantic settings and requirements to deployment

--- foothouse/pref/deployment.py
+++ foothouse/pref/deployment.py
@@ -1,15 +1,32 @@
 from prefect import flow
+from pydantic_settings import BaseSettings
 
-SOURCE_REPO = "https://github.com/chonalchendo/football-data-warehouse.git"
-SEASON = 2024
+from foothouse.utils import read_requirements
+
+# SOURCE_REPO = "https://github.com/chonalchendo/football-data-warehouse.git"
+
+
+class DeployConfig(BaseSettings):
+    SOURCE_REPO: str = (
+        "https://github.com/chonalchendo/football-data-warehouse/tree/development.git"
+    )
+    SEASON: int = 2024
+    PARAMETERS: dict = dict(season=SEASON)
+    DEPLOY_NAME: str = "transfermarkt-crawler"
+    ENTRYPOINT: str = "foothouse/pref/flows.py:transfermarkt_flow"
+    CRON: str = "0 4 * * TUE,FRI"
+    JOB_VARIABLES: dict = {"pip_packages": read_requirements()}
+    WORK_POOL_NAME: str = "my-work-pool"
+
 
 if __name__ == "__main__":
     flow.from_source(
-        source=SOURCE_REPO,
-        entrypoint="foothouse/pref/flows.py:transfermarkt_flow",
+        source=DeployConfig.SOURCE_REPO,
+        entrypoint=DeployConfig.ENTRYPOINT,
     ).deploy(
-        name="transfermarkt-crawler",
-        parameters=dict(season=SEASON),
-        work_pool_name="my-work-pool",
-        cron="0 4 * * TUE,FRI",
+        name=DeployConfig.DEPLOY_NAME,
+        parameters=DeployConfig.PARAMETERS,
+        work_pool_name=DeployConfig.WORK_POOL_NAME,
+        cron=DeployConfig.CRON,
+        job_variables=DeployConfig.JOB_VARIABLES,
     )
