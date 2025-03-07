665380c019eb99d46f41083138f9f20e1d77fc4b
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-07 15:20:54 +0000

    removed pydantic settings as it does not work

--- foothouse/pref/deployment.py
+++ foothouse/pref/deployment.py
@@ -3,31 +3,24 @@ from pydantic_settings import BaseSettings
 
 from foothouse.utils import read_requirements
 
-# SOURCE_REPO = "https://github.com/chonalchendo/football-data-warehouse.git"
-SOURCE_REPO = (
-    "https://github.com/chonalchendo/football-data-warehouse/tree/development.git"
-)
-
-
-class DeployConfig(BaseSettings):
-    SOURCE_REPO: str = SOURCE_REPO
-    SEASON: int = 2024
-    PARAMETERS: dict = dict(season=SEASON)
-    DEPLOY_NAME: str = "transfermarkt-crawler"
-    ENTRYPOINT: str = "foothouse/pref/flows.py:transfermarkt_flow"
-    CRON: str = "0 4 * * TUE,FRI"
-    JOB_VARIABLES: dict = {"pip_packages": read_requirements()}
-    WORK_POOL_NAME: str = "my-work-pool"
+SOURCE_REPO: str = "https://github.com/chonalchendo/football-data-warehouse.git"
+ENTRYPOINT: str = "foothouse/pref/flows.py:transfermarkt_flow"
+SEASON: int = 2024
+PARAMETERS: dict = dict(season=SEASON)
+DEPLOY_NAME: str = "transfermarkt-crawler"
+CRON: str = "0 4 * * TUE,FRI"
+JOB_VARIABLES: dict = {"pip_packages": read_requirements()}
+WORK_POOL_NAME: str = "my-work-pool"
 
 
 if __name__ == "__main__":
     flow.from_source(
-        source=DeployConfig.SOURCE_REPO,
-        entrypoint=DeployConfig.ENTRYPOINT,
+        source=SOURCE_REPO,
+        entrypoint=ENTRYPOINT,
     ).deploy(
-        name=DeployConfig.DEPLOY_NAME,
-        parameters=DeployConfig.PARAMETERS,
-        work_pool_name=DeployConfig.WORK_POOL_NAME,
-        cron=DeployConfig.CRON,
-        job_variables=DeployConfig.JOB_VARIABLES,
+        name=DEPLOY_NAME,
+        parameters=PARAMETERS,
+        work_pool_name=WORK_POOL_NAME,
+        cron=CRON,
+        job_variables=JOB_VARIABLES,
     )
