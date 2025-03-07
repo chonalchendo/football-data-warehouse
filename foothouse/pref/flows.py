06c389b3ee7c18268b4e4038f311d1db103ef5ca
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2025-03-07 16:17:33 +0000

    reduced flow to just clubs for testing - scrapy engine conflicts with prefect meaning once it has finished clubs it does not restart again for squads

--- foothouse/pref/flows.py
+++ foothouse/pref/flows.py
@@ -1,13 +1,25 @@
-from prefect import flow
+from prefect import flow, task
 
-from foothouse.pref.tasks.transfermarkt import (
-    run_transfermarkt_clubs_spider,
-    run_transfermarkt_squads_spider,
-)
+from foothouse.extractors.transfermarkt import run_clubs_spider, run_squads_spider
 
 
-@flow
+@flow(name="Transfermarkt Flow", log_prints=True)
 def transfermarkt_flow(season: int):
-    print("Running transfermarkt flow")
-    run_transfermarkt_clubs_spider(season=season)
-    run_transfermarkt_squads_spider(season=season)
+    print("Running transfermarkt squads spider")
+    run_clubs_spider(crawler="squads", season=season)
+
+
+# @task(name="Transfermarkt Squads Crawler", log_prints=True)
+# def run_transfermarkt_squads_spider(season: int) -> None:
+#     print("Running transfermarkt squads spider")
+#     run_squads_spider(crawler="squads", season=season)
+#
+#
+# @task(name="Transfermarkt Clubs Crawler", log_prints=True)
+# def run_transfermarkt_clubs_spider(season: int) -> None:
+#     print("Running transfermarkt clubs spider")
+#     run_clubs_spider(crawler="clubs", season=season)
+
+
+# if __name__ == "__main__":
+#     transfermarkt_flow(2024)
